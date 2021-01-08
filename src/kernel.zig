c9611de9cc69df7394f5cda8d66cb2c9eb3665dd
Amy Larane <amylarane@outlook.com>
2021-01-08 16:09:28 -0600

    Moved more code into other files and split the main method into functions

--- src/kernel.zig
+++ src/kernel.zig
@@ -1,57 +1,42 @@
 const std = @import("std");
-const uefi = std.os.uefi;
 
 const console = @import("efi/output/console.zig");
 const memory = @import("efi/memory.zig");
 
-pub fn main() void {    
+const uefi = std.os.uefi;
+
+const Allocator = std.mem.Allocator;
+const Writer = console.Writer;
+
+pub fn main() noreturn {    
     console.setup_screen();
     
-    var buffer = memory.getBuffer(8192);    
-    var allocator = &std.heap.FixedBufferAllocator.init(buffer).allocator;    
-    const writer = console.getWriter(allocator);
+    var allocator = memory.getAllocator(8192);
+    var writer = console.getWriter(allocator);
     
-    try std.fmt.format(writer, "Hello World!\r\n\r\n", .{});
+    printBootMessage(writer);    
+    eventLoop(writer, allocator);
+}
+
+pub fn printBootMessage(writer: Writer) void {
+    try std.fmt.format(writer, "Hello World!\n\n\r", .{});
     try std.fmt.format(writer, "Options:\r\n", .{});
     try std.fmt.format(writer, "    Press 'r' to reboot\r\n", .{});
-    try std.fmt.format(writer, "    Press 's' to shutdown\r\n", .{});
-    
+    try std.fmt.format(writer, "    Press 's' to shutdown\r\n", .{}); 
+}
+
+pub fn eventLoop(writer: Writer, allocator: *Allocator) noreturn {
     while (true) {
-        const time = uefi.system_table.runtime_services.*.getTime();
-        try console.writeAtPosition(writer,
-            40, 0,
-            "{d:0>2}:{d:0>2}:{d:0>2}",
-            .{time.hour,time.minute,time.second}
-        );
+        try console.writeAtPosition(writer, 40, 0, "{}", 
+            .{uefi.system_table.runtime_services.*.getTime()});
         
-        const reset = Reset.init(uefi.system_table.con_in.?.getKey());
+        const Reset = @import("efi/boot_manager/reset.zig").Reset;
+        const reset = Reset.init(std.os.uefi.system_table.con_in.?.getKey());
         if(reset.shouldReset){
-            memory.deleteBuffer(buffer);
+            memory.deleteAllocator(allocator);
             reset.reset();
         }
     }
 }
 
-pub const Reset = struct {
-    const ResetType = uefi.tables.ResetType;
-    shouldReset: bool,
-    rtype: ResetType,
-    
-    pub fn init(char: u16) Reset{
-        return Reset {
-            .shouldReset = (char == 's' or char == 'r'),
-            .rtype = switch(char){
-                's' => .ResetShutdown,
-                else => .ResetWarm,
-            },
-        };
-    }
-    
-    pub fn reset(self: Reset) void{
-        if(self.shouldReset){
-            uefi.system_table.runtime_services.
-                resetSystem(self.rtype, .Success, 0, null);
-        }
-    }
-};
 
