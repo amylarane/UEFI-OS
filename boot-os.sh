2a0947e898511f6ab8cab8a56f2d7b935b76a5da
Amy Larane <amylarane@gmail.com>
2021-01-12 14:51:54 -0600

    Modify ovmf usage

--- boot-os.sh
+++ boot-os.sh
@@ -1,3 +1,7 @@
 #! /bin/bash
 
-qemu-system-x86_64 -bios "OVMF.fd" -drive file=fat:rw:.,format=raw 2>/dev/null
+qemu-system-x86_64 -enable-kvm \
+	-m 2G \
+	-drive if=pflash,format=raw,readonly,file=./ovmf/OVMF_CODE.fd \
+	-drive if=pflash,format=raw,file=./ovmf/OVMF_VARS.fd \
+	-drive file=fat:rw:.,format=raw 2>/dev/null
