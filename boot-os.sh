8334285db4ef0e046b80da111b678776a5bf7ea0
amylarane <73141854+amylarane@users.noreply.github.com>
2021-01-13 09:34:30 -0600

    Update boot-os.sh

--- boot-os.sh
+++ boot-os.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-qemu-system-x86_64 -enable-kvm \
+qemu-system-x86_64 \
 	-m 2G \
 	-drive if=pflash,format=raw,readonly,file=./ovmf/OVMF_CODE.fd \
 	-drive if=pflash,format=raw,file=./ovmf/OVMF_VARS.fd \
